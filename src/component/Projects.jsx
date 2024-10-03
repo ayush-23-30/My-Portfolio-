c97182e3e1ff3dd99d9f6382809780ede74838c7
ayush-23-30 <pawar85060@gmail.com>
2024-10-03 16:29:23 +0530

    Card re-size

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -11,30 +11,33 @@ function Projects() {
       <h1 className="text-center sm:text-[2.5rem] text-[2rem] font-semibold uppercase tracking-wider md:text-[3.5rem]">
         Projects
       </h1>
-      <div className="flex items-center mt-7 flex-wrap gap-10 justify-evenly  ">
-        {ProjectData.map((data) => (
-          <div key={data.id} className="rounded-xl bg-[#151515] p-4 w-[85vw] md:w-[30vw] lg:w-[25vw] flex flex-col items-center">
+      <div className="flex items-center mt-7 flex-wrap gap-10 justify-evenly">
+  {ProjectData.map((data) => (
+    <div key={data.id} className="rounded-xl bg-[#151515] p-4 w-[85vw] md:w-[30vw] lg:w-[25vw] flex flex-col justify-between h-[400px]">
 
-            <img src={data.img} className='rounded-lg h-[190px]  w-full object- mb-4' alt={data.title} />
+      <img src={data.img} className='rounded-lg h-[190px] w-full object-cover mb-4' alt={data.title} />
 
-            <div className="flex justify-between items-center w-full">
-              <h3 className='text-xl md:text-2xl font-semibold text-[#00BFFF]'>
-                {data.title}
-              </h3>
-              <div className="flex ">
-              <a href={data.link} target='_blank' rel='noopener noreferrer'>
-                <FaGithub className='text-xl text-[#00BFFF]' />
-              </a>
-                <a href= {data.vercel} target='_blank' > <FaArrowRightToBracket className='text-xl ml-2 text-[#00BFFF]'/>  </a>
-
-              </div>
-            </div>
-            <p className='tracking-tighter text-sm md:text-base text-[#94A3B8]'>
-              {data.des}
-            </p>
-          </div>
-        ))}
+      <div className="flex justify-between items-center w-full">
+        <h3 className='text-xl md:text-2xl font-semibold text-[#00BFFF]'>
+          {data.title}
+        </h3>
+        <div className="flex">
+          <a href={data.link} target='_blank' rel='noopener noreferrer'>
+            <FaGithub className='text-xl text-[#00BFFF]' />
+          </a>
+          <a href={data.vercel} target='_blank'> 
+            <FaArrowRightToBracket className='text-xl ml-2 text-[#00BFFF]' />
+          </a>
+        </div>
       </div>
+
+      <p className='tracking-tighter text-sm md:text-base text-[#94A3B8] flex-grow'>
+        {data.des}
+      </p>
+    </div>
+  ))}
+</div>
+
     </div>
   );
 }
